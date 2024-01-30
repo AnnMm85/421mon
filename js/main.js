b6e7711f10a23e846405cdcadb185b38e78e16e3
AnnMm85 <8mona854@gmail.com>
2024-01-30 11:27:56 +0700

    Practic 10

--- js/main.js
+++ js/main.js
@@ -1,3 +1,88 @@
+Vue.component('product-review', {
+    template: `
+   <form class="review-form" @submit.prevent="onSubmit">
+ <p>
+ <p v-if="errors.length">
+ <b>Please correct the following error(s):</b>
+ <ul>
+   <li v-for="error in errors">{{ error }}</li>
+ </ul>
+</p>
+
+   <label for="name">Name:</label>
+   <input id="name" v-model="name" placeholder="name">
+ </p>
+
+ <p>
+   <label for="review">Review:</label>
+   <textarea id="review" v-model="review"></textarea>
+ </p>
+
+ <p>
+   <label for="rating">Rating:</label>
+   <select id="rating" v-model.number="rating">
+     <option>5</option>
+     <option>4</option>
+     <option>3</option>
+     <option>2</option>
+     <option>1</option>
+   </select>
+ </p>
+
+<p>
+  <fieldset>
+    <legend>Would you recommend this product?:</legend>
+    <div>
+      <input type="radio" id="yes_no_radio1" v-model="yes_no_radio" name="yes_no" value="I recommend" />
+      <label for="yes_no_radio1">Yes</label>
+
+      <input type="radio" id="yes_no_radio2" v-model="yes_no_radio" name="yes_no" value="I don't recommend it" />
+      <label for="yes_no_radio2">No</label>
+    </div>
+  </fieldset>
+</p>
+ <p>
+   <input type="submit" value="Submit"> 
+ </p>
+
+</form>
+
+ `,
+    data() {
+        return {
+            name: null,
+            review: null,
+            rating: null,
+            yes_no_radio: null,
+            errors: [],
+        }
+    },
+    methods:{
+        onSubmit() {
+            if(this.name && this.review && this.rating && this.yes_no_radio) {
+                let productReview = {
+                    name: this.name,
+                    review: this.review,
+                    rating: this.rating,
+                    yes_no_radio: this.yes_no_radio,
+                }
+                this.$emit('review-submitted', productReview)
+                this.name = null
+                this.review = null
+                this.rating = null
+                this.yes_no_radio = null
+            } else {
+                if(!this.name) this.errors.push("Name required.")
+                if(!this.review) this.errors.push("Review required.")
+                if(!this.rating) this.errors.push("Rating required.")
+                if(!this.yes_no_radio) this.errors.push("Recommend required.")
+            }
+        }
+    },
+})
+
+
+
 Vue.component('product', {
     props: {
         premium: {
@@ -34,14 +119,20 @@ Vue.component('product', {
            >
                Add to cart
            </button>
-           <button
-                   v-on:click="delToCart"
-                   :disabled="!inStock"
-                   :class="{ disabledButton: !inStock }"
-           >
-               Delete to cart
-           </button>       
        </div>
+       <div>
+            <h2>Reviews</h2>
+            <p v-if="!reviews.length">There are no reviews yet.</p>
+            <ul>
+              <li v-for="review in reviews">
+              <p>{{ review.name }}</p>
+              <p>Rating: {{ review.rating }}</p>
+              <p>{{ review.review }}</p>
+              <p>{{ review.yes_no_radio }}</p>
+              </li>
+            </ul>
+       </div>
+       <product-review @review-submitted="addReview"></product-review>
    </div>
  `,
     data() {
@@ -65,19 +156,20 @@ Vue.component('product', {
                     variantQuantity: 0
                 }
             ],
+            reviews: [],
         }
     },
     methods: {
         addToCart() {
             this.$emit('add-to-cart', this.variants[this.selectedVariant].variantId);
         },
-        delToCart() {
-            this.$emit('del-to-cart', this.variants[this.selectedVariant].variantId);
-        },
         updateProduct(index) {
             this.selectedVariant = index;
             console.log(index);
-        }
+        },
+        addReview(productReview) {
+            this.reviews.push(productReview)
+        },
     },
     computed: {
         title() {
@@ -102,14 +194,12 @@ let app = new Vue({
     el: '#app',
     data: {
         premium: true,
-        cart: [],
+        cart: []
     },
     methods: {
         updateCart(id) {
             this.cart.push(id);
         },
-        deleteCart(id) {
-            this.cart.pop(id);
-        },
-    },
+
+    }
 })
